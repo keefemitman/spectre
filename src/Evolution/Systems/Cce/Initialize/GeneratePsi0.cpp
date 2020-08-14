3dc8345eab21263ec56675c84f013b7ec9507afd
keefemitman <kmitman@caltech.edu>
2020-08-14 12:12:46 -0500

    radially solve psi_0 eq

--- src/Evolution/Systems/Cce/Initialize/GeneratePsi0.cpp
+++ src/Evolution/Systems/Cce/Initialize/GeneratePsi0.cpp
@@ -178,7 +178,7 @@ void radial_evolve_psi0_condition(
   const auto psi_0_condition_system =
       [](const std::array<ComplexDataVector, 2>& bondi_j_and_i,
          std::array<ComplexDataVector, 2>& dy_j_and_dy_i,
-         std::array<ComplexDataVector, 0>& psi_0
+         std::array<ComplexDataVector, 0>& psi_0,
          const double y) noexcept {
         dy_j_and_dy_i[0] = bondi_j_and_i[1];
         const auto& bondi_j = bondi_j_and_i[0];
