ba945fd32d341f5c31234e527540218d5f4c24e9
keefemitman <kmitman@caltech.edu>
2020-08-17 16:52:50 -0500

    fixing Psi0 bug

--- src/Evolution/Systems/Cce/Initialize/GeneratePsi0.cpp
+++ src/Evolution/Systems/Cce/Initialize/GeneratePsi0.cpp
@@ -187,7 +187,7 @@ void radial_evolve_psi0_condition(
         const auto& bondi_j = bondi_j_and_i[0];
         const auto& bondi_i = bondi_j_and_i[1];
         dy_j_and_dy_i[1] =
-            8.0 * (1.0 + sqrt(1.0 + conj(bondi_j) * bondi_j)) * square(r) *
+            - 8.0 * (1.0 + sqrt(1.0 + conj(bondi_j) * bondi_j)) * square(r) *
             (conj(psi_0) * square(bondi_j)
              / (2.0 + conj(bondi_j) * bondi_j +
                 2.0 * sqrt(1.0 + conj(bondi_j) * bondi_j)) +
