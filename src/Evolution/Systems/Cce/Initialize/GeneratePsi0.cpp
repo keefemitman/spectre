3aeba989ac553d5be10ed01cbbdc1b1433e64c25
keefemitman <kmitman@caltech.edu>
2020-08-21 16:21:50 -0500

    searching for Psi0 bug

--- src/Evolution/Systems/Cce/Initialize/GeneratePsi0.cpp
+++ src/Evolution/Systems/Cce/Initialize/GeneratePsi0.cpp
@@ -331,7 +331,7 @@ void GeneratePsi0::operator()(
       (square(0.5 * get(r_at_radius))
           * get(dr_dr_j_at_radius)).data()};
   Scalar<SpinWeighted<ComplexDataVector, 2>> dy_dy_j_at_radius_approx{
-      (-(j_at_radius / 2.0 + dy_j_at_radius)).data()};
+      (-(get(j_at_radius) / 2.0 + get(dy_j_at_radius))).data()};
 
   // compute psi_0
   Scalar<SpinWeighted<ComplexDataVector, 0>> one_minus_y{
