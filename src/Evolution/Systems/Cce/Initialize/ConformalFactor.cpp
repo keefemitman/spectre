fd92153da5734f59cb78a9903510f487177e9634
Keefe Mitman <kmitman@caltech.edu>
2021-03-11 11:47:38 -0800

    changed iterative solve to integration solve

--- src/Evolution/Systems/Cce/Initialize/ConformalFactor.cpp
+++ src/Evolution/Systems/Cce/Initialize/ConformalFactor.cpp
@@ -4,8 +4,13 @@
 #include "Evolution/Systems/Cce/Initialize/ConformalFactor.hpp"
 
 #include <cstddef>
+#include <cmath>
 #include <memory>
 #include <type_traits>
+
+#include <iostream>
+#include <fstream>
+#include <string>
 
 #include "DataStructures/ComplexDataVector.hpp"
 #include "DataStructures/DataVector.hpp"
@@ -16,6 +21,7 @@
 #include "DataStructures/Tensor/TypeAliases.hpp"
 #include "DataStructures/Variables.hpp"
 #include "Evolution/Systems/Cce/GaugeTransformBoundaryData.hpp"
+#include "NumericalAlgorithms/LinearOperators/IndefiniteIntegral.hpp"
 #include "NumericalAlgorithms/Spectral/Spectral.hpp"
 #include "NumericalAlgorithms/Spectral/SwshCollocation.hpp"
 #include "NumericalAlgorithms/Spectral/SwshDerivatives.hpp"
@@ -29,6 +35,7 @@
 
 namespace Cce::InitializeJ {
 namespace detail {
+
 double adjust_angular_coordinates_for_omega(
     const gsl::not_null<Scalar<SpinWeighted<ComplexDataVector, 2>>*> volume_j,
     const gsl::not_null<tnsr::i<DataVector, 3>*> cartesian_cauchy_coordinates,
@@ -40,6 +47,10 @@ double adjust_angular_coordinates_for_omega(
     const bool adjust_volume_gauge) noexcept {
   const size_t number_of_angular_points =
       Spectral::Swsh::number_of_swsh_collocation_points(l_max);
+  const size_t number_of_phi_points =
+      Spectral::Swsh::number_of_swsh_phi_collocation_points(l_max);
+  const size_t number_of_theta_points =
+      Spectral::Swsh::number_of_swsh_theta_collocation_points(l_max);
 
   const auto& collocation = Spectral::Swsh::cached_collocation_metadata<
       Spectral::Swsh::ComplexRepresentation::Interleaved>(l_max);
@@ -49,6 +60,88 @@ double adjust_angular_coordinates_for_omega(
     get<1>(*angular_cauchy_coordinates)[collocation_point.offset] =
         collocation_point.phi;
   }
+
+  Variables<tmpl::list<
+      // gauge Jacobians
+      ::Tags::SpinWeighted<::Tags::TempScalar<0, ComplexDataVector>,
+                           std::integral_constant<int, 2>>,
+      ::Tags::SpinWeighted<::Tags::TempScalar<1, ComplexDataVector>,
+                           std::integral_constant<int, 0>>,
+      // gauge conformal factor
+      ::Tags::SpinWeighted<::Tags::TempScalar<2, ComplexDataVector>,
+                           std::integral_constant<int, 0>>,
+      // Integral Input
+      ::Tags::SpinWeighted<::Tags::TempScalar<3, ComplexDataVector>,
+                           std::integral_constant<int, 0>>,
+      // Integral Result
+      ::Tags::SpinWeighted<::Tags::TempScalar<4, ComplexDataVector>,
+                           std::integral_constant<int, 0>>>>
+    computation_buffers{number_of_angular_points};
+
+  auto& gauge_c =
+      get<::Tags::SpinWeighted<::Tags::TempScalar<0, ComplexDataVector>,
+                               std::integral_constant<int, 2>>>(
+          computation_buffers);
+  auto& gauge_d =
+      get<::Tags::SpinWeighted<::Tags::TempScalar<1, ComplexDataVector>,
+                               std::integral_constant<int, 0>>>(
+          computation_buffers);
+
+  auto& gauge_omega =
+      get<::Tags::SpinWeighted<::Tags::TempScalar<2, ComplexDataVector>,
+                               std::integral_constant<int, 0>>>(
+          computation_buffers);
+
+  // Compute the Integrand
+  auto& integral_input =
+      get<::Tags::SpinWeighted<::Tags::TempScalar<3, ComplexDataVector>,
+                               std::integral_constant<int, 0>>>(
+          computation_buffers);
+
+  // Note that there is no factor of sin(\theta) because this is
+  // removed to convert the integration to the angular domain.
+  get(integral_input).data() =
+      pow(target_omega.data(), 2.0);
+
+  // Obtain theta via Integration (loop over the phis)
+  auto& integral_result =
+      get<::Tags::SpinWeighted<::Tags::TempScalar<4, ComplexDataVector>,
+                               std::integral_constant<int, 0>>>(
+          computation_buffers);
+
+  const Mesh<2> Mesh2d{
+      {{number_of_phi_points, number_of_theta_points}},
+      {{Spectral::Basis::Legendre, Spectral::Basis::Legendre}},
+      {{Spectral::Quadrature::Gauss, Spectral::Quadrature::Gauss}}};
+  indefinite_integral(make_not_null(&get(integral_result).data()),
+                      get(integral_input).data(),
+                      Mesh2d,
+                      1);
+
+  // this is just for debugging
+  Parallel::printf("\n");
+  Parallel::printf("Omega Min Max: %e %e\n",
+      min(real(target_omega.data())),
+      max(real(target_omega.data())));
+  Parallel::printf("Integrand Min Max: %e %e\n",
+      min(real(get(integral_input).data())),
+      max(real(get(integral_input).data())));
+  Parallel::printf("Integral Min Max: %e %e\n",
+      min(real(get(integral_result).data())),
+      max(real(get(integral_result).data())));
+  Parallel::printf("Result Min Max: %e %e\n",
+      min(-real(get(integral_result).data()) + 1.0),
+      max(-real(get(integral_result).data()) + 1.0));
+  Parallel::printf("ArcCos Min Max: %e %e\n",
+      acos(max(-real(get(integral_result).data()) + 1.0)),
+      acos(min(-real(get(integral_result).data()) + 1.0)));
+
+  for (size_t i = 0; i < get<0>(*angular_cauchy_coordinates).size(); ++i) {
+    get<0>(*angular_cauchy_coordinates)[i] =
+      acos(-real(get(integral_result).data()[i]) + 1.0);
+  }
+
+  // Update the Cartesian coordinates
   get<0>(*cartesian_cauchy_coordinates) =
       sin(get<0>(*angular_cauchy_coordinates)) *
       cos(get<1>(*angular_cauchy_coordinates));
@@ -58,238 +151,35 @@ double adjust_angular_coordinates_for_omega(
   get<2>(*cartesian_cauchy_coordinates) =
       cos(get<0>(*angular_cauchy_coordinates));
 
-  Variables<tmpl::list<
-      // cartesian coordinates
-      ::Tags::SpinWeighted<::Tags::TempScalar<0, ComplexDataVector>,
-                           std::integral_constant<int, 0>>,
-      ::Tags::SpinWeighted<::Tags::TempScalar<1, ComplexDataVector>,
-                           std::integral_constant<int, 0>>,
-      ::Tags::SpinWeighted<::Tags::TempScalar<2, ComplexDataVector>,
-                           std::integral_constant<int, 0>>,
-      // eth of cartesian coordinates
-      ::Tags::SpinWeighted<::Tags::TempScalar<3, ComplexDataVector>,
-                           std::integral_constant<int, 1>>,
-      ::Tags::SpinWeighted<::Tags::TempScalar<4, ComplexDataVector>,
-                           std::integral_constant<int, 1>>,
-      ::Tags::SpinWeighted<::Tags::TempScalar<5, ComplexDataVector>,
-                           std::integral_constant<int, 1>>,
-      // eth of gauge-transformed cartesian coordinates
-      ::Tags::SpinWeighted<::Tags::TempScalar<6, ComplexDataVector>,
-                           std::integral_constant<int, 1>>,
-      ::Tags::SpinWeighted<::Tags::TempScalar<7, ComplexDataVector>,
-                           std::integral_constant<int, 1>>,
-      ::Tags::SpinWeighted<::Tags::TempScalar<8, ComplexDataVector>,
-                           std::integral_constant<int, 1>>,
-      // gauge Jacobians
-      ::Tags::SpinWeighted<::Tags::TempScalar<12, ComplexDataVector>,
-                           std::integral_constant<int, 2>>,
-      ::Tags::SpinWeighted<::Tags::TempScalar<13, ComplexDataVector>,
-                           std::integral_constant<int, 0>>,
-      // gauge Jacobians on next iteration
-      ::Tags::SpinWeighted<::Tags::TempScalar<14, ComplexDataVector>,
-                           std::integral_constant<int, 2>>,
-      ::Tags::SpinWeighted<::Tags::TempScalar<15, ComplexDataVector>,
-                           std::integral_constant<int, 0>>,
-      // gauge conformal factor
-      ::Tags::SpinWeighted<::Tags::TempScalar<16, ComplexDataVector>,
-                           std::integral_constant<int, 0>>,
-      // cartesian coordinates steps
-      ::Tags::SpinWeighted<::Tags::TempScalar<17, ComplexDataVector>,
-                           std::integral_constant<int, 0>>,
-      ::Tags::SpinWeighted<::Tags::TempScalar<18, ComplexDataVector>,
-                           std::integral_constant<int, 0>>,
-      ::Tags::SpinWeighted<::Tags::TempScalar<19, ComplexDataVector>,
-                           std::integral_constant<int, 0>>>>
-      computation_buffers{number_of_angular_points};
-
-  auto& x =
-      get(get<::Tags::SpinWeighted<::Tags::TempScalar<0, ComplexDataVector>,
-                                   std::integral_constant<int, 0>>>(
-          computation_buffers));
-  auto& y =
-      get(get<::Tags::SpinWeighted<::Tags::TempScalar<1, ComplexDataVector>,
-                                   std::integral_constant<int, 0>>>(
-          computation_buffers));
-  auto& z =
-      get(get<::Tags::SpinWeighted<::Tags::TempScalar<2, ComplexDataVector>,
-                                   std::integral_constant<int, 0>>>(
-          computation_buffers));
-
-  x.data() =
-      std::complex<double>(1.0, 0.0) * get<0>(*cartesian_cauchy_coordinates);
-  y.data() =
-      std::complex<double>(1.0, 0.0) * get<1>(*cartesian_cauchy_coordinates);
-  z.data() =
-      std::complex<double>(1.0, 0.0) * get<2>(*cartesian_cauchy_coordinates);
-
-  auto& eth_x =
-      get(get<::Tags::SpinWeighted<::Tags::TempScalar<3, ComplexDataVector>,
-                                   std::integral_constant<int, 1>>>(
-          computation_buffers));
-  auto& eth_y =
-      get(get<::Tags::SpinWeighted<::Tags::TempScalar<4, ComplexDataVector>,
-                                   std::integral_constant<int, 1>>>(
-          computation_buffers));
-  auto& eth_z =
-      get(get<::Tags::SpinWeighted<::Tags::TempScalar<5, ComplexDataVector>,
-                                   std::integral_constant<int, 1>>>(
-          computation_buffers));
-
-  Spectral::Swsh::angular_derivatives<
-      tmpl::list<Spectral::Swsh::Tags::Eth, Spectral::Swsh::Tags::Eth,
-                 Spectral::Swsh::Tags::Eth>>(l_max, 1, make_not_null(&eth_x),
-                                             make_not_null(&eth_y),
-                                             make_not_null(&eth_z), x, y, z);
-
-  auto& evolution_gauge_eth_x =
-      get(get<::Tags::SpinWeighted<::Tags::TempScalar<6, ComplexDataVector>,
-                                   std::integral_constant<int, 1>>>(
-          computation_buffers));
-  auto& evolution_gauge_eth_y =
-      get(get<::Tags::SpinWeighted<::Tags::TempScalar<7, ComplexDataVector>,
-                                   std::integral_constant<int, 1>>>(
-          computation_buffers));
-  auto& evolution_gauge_eth_z =
-      get(get<::Tags::SpinWeighted<::Tags::TempScalar<8, ComplexDataVector>,
-                                   std::integral_constant<int, 1>>>(
-          computation_buffers));
-
-  auto& gauge_c =
-      get<::Tags::SpinWeighted<::Tags::TempScalar<12, ComplexDataVector>,
-                               std::integral_constant<int, 2>>>(
-          computation_buffers);
-  auto& gauge_d =
-      get<::Tags::SpinWeighted<::Tags::TempScalar<13, ComplexDataVector>,
-                               std::integral_constant<int, 0>>>(
-          computation_buffers);
-
-  auto& next_gauge_c =
-      get<::Tags::SpinWeighted<::Tags::TempScalar<14, ComplexDataVector>,
-                               std::integral_constant<int, 2>>>(
-          computation_buffers);
-  auto& next_gauge_d =
-      get<::Tags::SpinWeighted<::Tags::TempScalar<15, ComplexDataVector>,
-                               std::integral_constant<int, 0>>>(
-          computation_buffers);
-
-  auto& gauge_omega =
-      get<::Tags::SpinWeighted<::Tags::TempScalar<16, ComplexDataVector>,
-                               std::integral_constant<int, 0>>>(
-          computation_buffers);
-
-  auto& x_step =
-      get(get<::Tags::SpinWeighted<::Tags::TempScalar<17, ComplexDataVector>,
-                                   std::integral_constant<int, 0>>>(
-          computation_buffers));
-  auto& y_step =
-      get(get<::Tags::SpinWeighted<::Tags::TempScalar<18, ComplexDataVector>,
-                                   std::integral_constant<int, 0>>>(
-          computation_buffers));
-  auto& z_step =
-      get(get<::Tags::SpinWeighted<::Tags::TempScalar<19, ComplexDataVector>,
-                                   std::integral_constant<int, 0>>>(
-          computation_buffers));
-
-  double max_error = 1.0;
-  size_t number_of_steps = 0;
-  while (true) {
-    GaugeUpdateAngularFromCartesian<
-        Tags::CauchyAngularCoords,
-        Tags::CauchyCartesianCoords>::apply(angular_cauchy_coordinates,
-                                            cartesian_cauchy_coordinates);
-
-    Spectral::Swsh::SwshInterpolator iteration_interpolator{
-      get<0>(*angular_cauchy_coordinates),
-      get<1>(*angular_cauchy_coordinates), l_max};
-
-    GaugeUpdateJacobianFromCoordinates<
-        Tags::GaugeC, Tags::GaugeD, Tags::CauchyAngularCoords,
-        Tags::CauchyCartesianCoords>::apply(make_not_null(&gauge_c),
-                                            make_not_null(&gauge_d),
-                                            angular_cauchy_coordinates,
-                                            *cartesian_cauchy_coordinates,
-                                            l_max);
-
-    get(gauge_omega).data() =
-        0.5 * sqrt(get(gauge_d).data() * conj(get(gauge_d).data()) -
-                   get(gauge_c).data() * conj(get(gauge_c).data()));
-
-    // check completion conditions
-    max_error = max(abs(get(gauge_omega).data() - target_omega.data()));
-    Parallel::printf("Debug iterative solve: %e\n", max_error);
-    ++number_of_steps;
-    if (max_error > 5.0e-3) {
-      ERROR(
-          "Iterative solve for surface coordinates of initial data failed. The "
-          "strain is too large to be fully eliminated by a well-behaved "
-          "alteration of the spherical mesh. For this data, please use an "
-          "alternative initial data generator such as "
-          "`InitializeJConformalFactor`.");
-    }
-    if (max_error < tolerance or number_of_steps > max_steps) {
-      break;
-    }
-    // The alteration in each of the spin-weighted Jacobian factors determined
-    // by linearizing the system in small J
-    get(next_gauge_c).data() =
-        -(target_omega.data() - get(gauge_omega).data()) * get(gauge_c).data();
-    get(next_gauge_d).data() = get(gauge_omega).data() *
-                               (target_omega.data() - get(gauge_omega).data()) /
-                               get(gauge_d).data();
-
-    iteration_interpolator.interpolate(make_not_null(&evolution_gauge_eth_x),
-                                       eth_x);
-    iteration_interpolator.interpolate(make_not_null(&evolution_gauge_eth_y),
-                                       eth_y);
-    iteration_interpolator.interpolate(make_not_null(&evolution_gauge_eth_z),
-                                       eth_z);
-
-    evolution_gauge_eth_x =
-        0.5 * ((get(next_gauge_c)) * conj(evolution_gauge_eth_x) +
-               conj((get(next_gauge_d))) * evolution_gauge_eth_x);
-    evolution_gauge_eth_y =
-        0.5 * ((get(next_gauge_c)) * conj(evolution_gauge_eth_y) +
-               conj((get(next_gauge_d))) * evolution_gauge_eth_y);
-    evolution_gauge_eth_z =
-        0.5 * ((get(next_gauge_c)) * conj(evolution_gauge_eth_z) +
-               conj((get(next_gauge_d))) * evolution_gauge_eth_z);
-
-    // here we attempt to just update the current value according to the
-    // alteration suggested. Ideally that's the `dominant` part of the needed
-    // alteration
-    Spectral::Swsh::angular_derivatives<tmpl::list<
-        Spectral::Swsh::Tags::InverseEth, Spectral::Swsh::Tags::InverseEth,
-        Spectral::Swsh::Tags::InverseEth>>(
-        l_max, 1, make_not_null(&x_step), make_not_null(&y_step),
-        make_not_null(&z_step), evolution_gauge_eth_x, evolution_gauge_eth_y,
-        evolution_gauge_eth_z);
-
-    get<0>(*cartesian_cauchy_coordinates) += real(x_step.data());
-    get<1>(*cartesian_cauchy_coordinates) += real(y_step.data());
-    get<2>(*cartesian_cauchy_coordinates) += real(z_step.data());
-  }
-
+  // Update angular coordinates so they are formatted correctly,
+  // i.e., phi in [-pi, pi]
   GaugeUpdateAngularFromCartesian<
       Tags::CauchyAngularCoords,
       Tags::CauchyCartesianCoords>::apply(angular_cauchy_coordinates,
                                           cartesian_cauchy_coordinates);
 
-  if (adjust_volume_gauge) {
-    GaugeUpdateJacobianFromCoordinates<
-        Tags::GaugeC, Tags::GaugeD, Tags::CauchyAngularCoords,
-        Tags::CauchyCartesianCoords>::apply(make_not_null(&gauge_c),
-                                            make_not_null(&gauge_d),
-                                            angular_cauchy_coordinates,
-                                            *cartesian_cauchy_coordinates,
-                                            l_max);
+  // Use the new coordinates to create new gauge_c, gauge_d
+  GaugeUpdateJacobianFromCoordinates<
+      Tags::GaugeC, Tags::GaugeD, Tags::CauchyAngularCoords,
+      Tags::CauchyCartesianCoords>::apply(make_not_null(&gauge_c),
+                                          make_not_null(&gauge_d),
+                                          angular_cauchy_coordinates,
+                                          *cartesian_cauchy_coordinates,
+                                          l_max);
 
-    get(gauge_omega).data() =
-        0.5 * sqrt(get(gauge_d).data() * conj(get(gauge_d).data()) -
-                   get(gauge_c).data() * conj(get(gauge_c).data()));
+  // Compute omega and check the error
+  get(gauge_omega).data() =
+      0.5 * sqrt(get(gauge_d).data() * conj(get(gauge_d).data()) -
+                 get(gauge_c).data() * conj(get(gauge_c).data()));
 
-    GaugeAdjustInitialJ::apply(volume_j, gauge_c, gauge_d, gauge_omega,
-                               *angular_cauchy_coordinates, l_max);
-  }
+  double max_error = 1.0;
+  max_error = max(abs(get(gauge_omega).data() - target_omega.data()));
+
+  // then run
+  GaugeAdjustInitialJ::apply(volume_j, gauge_c, gauge_d, gauge_omega,
+                             *angular_cauchy_coordinates, l_max);
+
+  Parallel::printf("Integral solve: %e\n", max_error);
   return max_error;
 }
 }  // namespace detail
--- src/NumericalAlgorithms/Spectral/Spectral.hpp
+++ src/NumericalAlgorithms/Spectral/Spectral.hpp
@@ -141,7 +141,7 @@ constexpr size_t minimum_number_of_points =
  */
 template <Basis basis>
 constexpr size_t maximum_number_of_points =
-    basis == Basis::FiniteDifference ? 23 : 12;
+    basis == Basis::FiniteDifference ? 50 : 50;
 
 /*!
  * \brief Compute the function values of the basis function \f$\Phi_k(x)\f$
